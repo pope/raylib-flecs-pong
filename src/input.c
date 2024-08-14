922f1e5b87ecf7f8cc6d395b1da9c585e7da85df
K. Adam Christensen <pope@shifteleven.com>
2024-08-13 20:02:14 -0700

    Add a pause to the game

--- src/input.c
+++ src/input.c
@@ -1,11 +1,23 @@
 #include "input.h"
 
-ECS_COMPONENT_DECLARE (KeyBinding);
+#include <raylib.h>
+
+static void
+CheckPause (ecs_iter_t *it)
+{
+  if (IsKeyPressed (KEY_P))
+    {
+      if (ecs_has_id (it->world, EcsOnUpdate, EcsDisabled))
+        ecs_remove_id (it->world, EcsOnUpdate, EcsDisabled);
+      else
+        ecs_add_id (it->world, EcsOnUpdate, EcsDisabled);
+    }
+}
 
 void
 InputModuleImport (ecs_world_t *world)
 {
   ECS_MODULE (world, InputModule);
 
-  ECS_COMPONENT_DEFINE (world, KeyBinding);
+  ECS_SYSTEM (world, CheckPause, EcsPreUpdate, 0);
 }
--- src/input.h
+++ src/input.h
@@ -1,13 +1,5 @@
 #pragma once
 
 #include <flecs.h>
-#include <raylib.h>
-
-typedef struct
-{
-  KeyboardKey key;
-} KeyBinding;
-
-extern ECS_COMPONENT_DECLARE (KeyBinding);
 
 void InputModuleImport (ecs_world_t *world);
