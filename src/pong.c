ba9f46f3de089d055e121941cd1a80d442bd41ee
K. Adam Christensen <pope@shifteleven.com>
2024-08-15 23:57:07 -0700

    Add some paddles

--- src/pong.c
+++ src/pong.c
@@ -1,10 +1,23 @@
 #include "pong.h"
-#include "raylib.h"
 
-ECS_COMPONENT_DECLARE (Position);
-ECS_COMPONENT_DECLARE (Velocity);
+#include <raylib.h>
+#include <sys/param.h>
 
-ECS_TAG_DECLARE (Ball);
+#define PADDLE_WIDTH 30
+#define PADDLE_HEIGHT 160
+#define PADDLE_GAP 10
+
+#define BALL_RADIUS 20
+
+#define CLAMP(v, min, max) (MIN ((max), MAX ((min), (v))))
+
+typedef Vector2 Position;
+typedef Vector2 Velocity;
+
+ECS_STRUCT (Player, {
+  int32_t up_key;
+  int32_t down_key;
+});
 
 static ecs_world_t *world;
 
@@ -21,7 +34,7 @@ CheckPause (ecs_iter_t *it)
 }
 
 static void
-Move (ecs_iter_t *it)
+MoveBall (ecs_iter_t *it)
 {
   Position *p = ecs_field (it, Position, 0);
   Velocity *v = ecs_field (it, Velocity, 1);
@@ -31,25 +44,77 @@ Move (ecs_iter_t *it)
       p[i].x += v[i].x;
       p[i].y += v[i].y;
 
-      if (p[i].x + 20.0f >= WINDOW_WIDTH || p[i].x - 20.0f <= 0)
+      if ((p[i].x + BALL_RADIUS >= WINDOW_WIDTH) | (p[i].x - BALL_RADIUS <= 0))
         v[i].x *= -1;
-      if (p[i].y + 20.0f >= WINDOW_HEIGHT || p[i].y - 20.0f <= 0)
+      if ((p[i].y + BALL_RADIUS >= WINDOW_HEIGHT)
+          | (p[i].y - BALL_RADIUS <= 0))
         v[i].y *= -1;
     }
 }
 
 static void
-Render (ecs_iter_t *it)
+MovePlayer (ecs_iter_t *it)
+{
+  Position *p = ecs_field (it, Position, 0);
+  const Velocity *v = ecs_field (it, Velocity, 1);
+  const Player *player = ecs_field (it, Player, 2);
+
+  for (int i = 0; i < it->count; i++)
+    {
+      char dir = 0;
+      if (IsKeyDown (player[i].up_key))
+        dir--;
+      if (IsKeyDown (player[i].down_key))
+        dir++;
+
+      p[i].y += v[i].y * dir;
+      p[i].y = CLAMP (p[i].y, PADDLE_GAP,
+                      WINDOW_HEIGHT - PADDLE_HEIGHT - PADDLE_GAP);
+    }
+}
+
+static void
+BeginRendering (ecs_iter_t *it)
+{
+  (void)it;
+  BeginDrawing ();
+  ClearBackground (BLACK);
+}
+
+static void
+EndRendering (ecs_iter_t *it)
+{
+  (void)it;
+  DrawFPS (WINDOW_WIDTH - 100, 20);
+  EndDrawing ();
+}
+
+static void
+RenderBackground (ecs_iter_t *it)
+{
+  (void)it;
+  DrawLine (WINDOW_WIDTH / 2, 0, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4, WHITE);
+  DrawLine (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 4 * 3, WINDOW_WIDTH / 2,
+            WINDOW_HEIGHT, WHITE);
+  DrawCircleLines (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, WINDOW_HEIGHT / 4.0,
+                   WHITE);
+}
+
+static void
+RenderBall (ecs_iter_t *it)
 {
   const Position *p = ecs_field (it, Position, 0);
+  for (int i = 0; i < it->count; i++)
+    DrawCircle ((int)p[i].x, (int)p[i].y, BALL_RADIUS, WHITE);
+}
+static void
 
-  BeginDrawing ();
-  {
-    ClearBackground (BLACK);
-    for (int i = 0; i < it->count; i++)
-      DrawCircle ((int)p[i].x, (int)p[i].y, 20.0, WHITE);
-  }
-  EndDrawing ();
+RenderPaddle (ecs_iter_t *it)
+{
+  const Position *p = ecs_field (it, Position, 0);
+  for (int i = 0; i < it->count; i++)
+    DrawRectangle ((int)p[i].x, (int)p[i].y, PADDLE_WIDTH, PADDLE_HEIGHT,
+                   WHITE);
 }
 
 void
@@ -57,33 +122,80 @@ setup_world (void)
 {
   world = ecs_init ();
 
-  ecs_set_threads (world, 8);
+  ecs_set_threads (world, 2);
   ecs_log_set_level (-1);
   ecs_log_enable_colors (true);
+
+  // Definitions
 
   ecs_singleton_set (world, EcsRest, { 0 });
   ECS_IMPORT (world, FlecsStats);
 
-  ECS_COMPONENT_DEFINE (world, Position);
+  ECS_COMPONENT (world, Position);
   ecs_struct (world,
               { .entity = ecs_id (Position),
                 .members = { { .name = "x", .type = ecs_id (ecs_f32_t) },
                              { .name = "y", .type = ecs_id (ecs_f32_t) } } });
-  ECS_COMPONENT_DEFINE (world, Velocity);
+  ECS_COMPONENT (world, Velocity);
   ecs_struct (world,
               { .entity = ecs_id (Velocity),
                 .members = { { .name = "x", .type = ecs_id (ecs_f32_t) },
                              { .name = "y", .type = ecs_id (ecs_f32_t) } } });
-  ECS_TAG_DEFINE (world, Ball);
+  ECS_META_COMPONENT (world, Player);
+  ECS_TAG (world, Ball);
+  ECS_TAG (world, Paddle);
+  ECS_TAG (world, Cpu);
 
-  ECS_SYSTEM (world, CheckPause, EcsPreUpdate, 0);
-  ECS_SYSTEM (world, Move, EcsOnUpdate, Position, Velocity, [none] Ball);
-  ECS_SYSTEM (world, Render, EcsOnStore, [in] Position, [none] Ball);
+  // Game objects
 
-  ecs_entity_t e = ecs_entity (world, { .name = "Ball#1" });
-  ecs_set (world, e, Position, { 100, 200 });
-  ecs_set (world, e, Velocity, { 2, 4 });
-  ecs_add_id (world, e, Ball);
+  {
+    ecs_entity_t e = ecs_entity (world, { .name = "Ball#1" });
+    ecs_set (world, e, Position, { WINDOW_WIDTH / 2.0, WINDOW_HEIGHT / 2.0 });
+    ecs_set (world, e, Velocity, { 6, 6 });
+    ecs_add_id (world, e, Ball);
+  }
+  {
+    ecs_entity_t e = ecs_entity (world, { .name = "Player#1" });
+    ecs_set (world, e, Position,
+             { PADDLE_GAP, WINDOW_HEIGHT / 2.0 - PADDLE_HEIGHT / 2.0 });
+    ecs_set (world, e, Velocity, { 0, 6 });
+    ecs_set (world, e, Player, { KEY_W, KEY_S });
+    ecs_add_id (world, e, Paddle);
+  }
+  {
+    ecs_entity_t e = ecs_entity (world, { .name = "Cpu#1" });
+    ecs_set (world, e, Position,
+             { WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_GAP,
+               WINDOW_HEIGHT / 2.0 - PADDLE_HEIGHT / 2.0 });
+    ecs_set (world, e, Velocity, { 0, 6 });
+    ecs_add_id (world, e, Paddle);
+    ecs_add_id (world, e, Cpu);
+  }
+
+  // Systems
+  {
+    ecs_entity_t PreRendering = ecs_new_w_id (world, EcsPhase);
+    ecs_set_name (world, PreRendering, "PreRendering");
+    ecs_entity_t OnRendering = ecs_new_w_id (world, EcsPhase);
+    ecs_set_name (world, OnRendering, "OnRendering");
+    ecs_entity_t PostRendering = ecs_new_w_id (world, EcsPhase);
+    ecs_set_name (world, PostRendering, "PostRendering");
+
+    ecs_add_pair (world, PreRendering, EcsDependsOn, EcsOnStore);
+    ecs_add_pair (world, OnRendering, EcsDependsOn, PreRendering);
+    ecs_add_pair (world, PostRendering, EcsDependsOn, OnRendering);
+
+    ECS_SYSTEM (world, CheckPause, EcsPreUpdate, 0);
+    ECS_SYSTEM (world, MovePlayer, EcsOnUpdate,
+                Position, [in] Velocity, [in] Player);
+    ECS_SYSTEM (world, MoveBall, EcsOnUpdate, Position, Velocity, [none] Ball);
+    ECS_SYSTEM (world, BeginRendering, PreRendering, 0);
+    ECS_SYSTEM (world, RenderBackground, OnRendering, 0);
+    ECS_SYSTEM (world, RenderBall, OnRendering, [in] Position, [none] Ball);
+    ECS_SYSTEM (world, RenderPaddle,
+                OnRendering, [in] Position, [none] Paddle);
+    ECS_SYSTEM (world, EndRendering, PostRendering, 0);
+  }
 }
 
 void
@@ -97,5 +209,6 @@ cleanup_world (void)
 bool
 run_game_loop (void)
 {
+  assert (world);
   return ecs_progress (world, 0.0);
 }
--- src/pong.h
+++ src/pong.h
@@ -4,17 +4,20 @@
 #include <raylib.h>
 #include <stdbool.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+// TODO(pope): Maybe we don't need these if we can use GetScreenWidth() and
+// GetScreenHeight()
 #define WINDOW_WIDTH 1280
 #define WINDOW_HEIGHT 720
 
-typedef Vector2 Position;
-typedef Vector2 Velocity;
+  void setup_world (void);
+  void cleanup_world (void);
+  bool run_game_loop (void);
 
-extern ECS_COMPONENT_DECLARE (Position);
-extern ECS_COMPONENT_DECLARE (Velocity);
-
-extern ECS_TAG_DECLARE (Ball);
-
-void setup_world (void);
-void cleanup_world (void);
-bool run_game_loop (void);
+#ifdef __cplusplus
+}
+#endif
