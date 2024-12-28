94018709f747c99112f734543e02c1bb783e2844
K. Adam Christensen <pope@shifteleven.com>
2024-12-28 14:04:17 -0800

    Add a nix-shell file
    Signed-off-by: K. Adam Christensen <pope@shifteleven.com>

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs = { nixpkgs, systems, treefmt-nix, ... }:
